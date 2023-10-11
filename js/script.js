fb2aaa69c2bf83684525d8ccd4e6dff4f65c28e7
Miguel Angel Moreno Moreno <miguelaangel325angel@gmail.com>
2023-10-11 13:45:34 -0500

    agregados script de bootstrap

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//img 360 //
+$(document).ready(function() {
+    $('.dropdown-toggle').dropdown();
+});
+
+// flex cards //
+$(".option").click(function(){
+    $(".option").removeClass("active");
+    $(this).addClass("active");
+    
+ });
